64d99db36a73da7cbc5d2105c9d0a1cb81b6c0c0
tegarswasono <tegar.s@weefer.co.id>
2021-10-04 12:10:36 +0700

    add ClosedXML package

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,5 +37,54 @@ namespace LearnClosedXML.Controllers
             })
             .ToArray();
         }
+        //https://www.infoworld.com/article/3538413/how-to-export-data-to-excel-in-aspnet-core-30.html
+        [HttpGet("Download")]
+        public ActionResult Download()
+        {
+            List<Author> authors = new List<Author>
+            {
+                new Author { Id = 1, FirstName = "Joydip", LastName = "Kanjilal" },
+                new Author { Id = 2, FirstName = "Steve", LastName = "Smith" },
+                new Author { Id = 3, FirstName = "Anand", LastName = "Narayaswamy"}
+            };
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            string fileName = "authors.xlsx";
+            try
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    IXLWorksheet worksheet = workbook.Worksheets.Add("Authors");
+                    worksheet.Cell(1, 1).Value = "Id";
+                    worksheet.Cell(1, 2).Value = "FirstName";
+                    worksheet.Cell(1, 3).Value = "LastName";
+
+                    int index = 2;
+                    foreach (var author in authors)
+                    {
+                        worksheet.Cell(index, 1).Value = author.Id;
+                        worksheet.Cell(index, 2).Value = author.FirstName;
+                        worksheet.Cell(index, 3).Value = author.LastName;
+                        index++;
+                    }
+
+                    using (var stream = new MemoryStream())
+                    {
+                        workbook.SaveAs(stream);
+                        var content = stream.ToArray();
+                        return File(content, contentType, fileName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+    public class Author  
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
