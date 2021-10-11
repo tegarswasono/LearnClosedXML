551f62eed7b574e981f2ee85e69ac91d8412493e
tegarswasono <tegar.s@weefer.co.id>
2021-10-11 09:10:26 +0700

    .

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -41,26 +41,16 @@ namespace LearnClosedXML.Controllers
         [HttpGet("Download")]
         public ActionResult Download()
         {
-            List<Author> authors = new List<Author>
-            {
-                new Author { Id = 1, FirstName = "Joydip", LastName = "Kanjilal" },
-                new Author { Id = 2, FirstName = "Steve jos jos jos", LastName = "Smith" },
-                new Author { Id = 3, FirstName = "Anand", LastName = "Narayaswamy"}
-            };
-            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            string fileName = "authors.xlsx";
             try
             {
-                using (var workbook = new XLWorkbook())
+                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                string fileName = "authors.xlsx";
+                var workbook = GenerateWorkbook();
+                using (var stream = new MemoryStream())
                 {
-                    GenerateBody(authors, workbook);
-
-                    using (var stream = new MemoryStream())
-                    {
-                        workbook.SaveAs(stream);
-                        var content = stream.ToArray();
-                        return File(content, contentType, fileName);
-                    }
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName);
                 }
             }
             catch (Exception ex)
@@ -69,45 +59,57 @@ namespace LearnClosedXML.Controllers
             }
         }
 
-        private static void GenerateBody(List<Author> authors, XLWorkbook workbook)
+        private static XLWorkbook GenerateWorkbook()
         {
+            List<Author> authors = new List<Author>
+                {
+                    new Author { Id = 1, FirstName = "Joydip", LastName = "Kanjilal" },
+                    new Author { Id = 2, FirstName = "Steve jos jos jos", LastName = "Smith" },
+                    new Author { Id = 3, FirstName = "Anand", LastName = "Narayaswamy"}
+                };
+
+            var workbook = new XLWorkbook();
             IXLWorksheet worksheet = workbook.Worksheets.Add("Authors");
             List<string> headers = new List<string>() { "Id", "FirstName", "LastName" };
-            int indexHeader = 1;
+            int rowHeader = 1;
+            int columnHeader = 1;
             foreach (var header in headers)
             {
-                worksheet.Cell(1, indexHeader).Value = header;
+                worksheet.Cell(1, columnHeader).Value = header;
 
                 //styling
-                worksheet.Range(worksheet.Cell(1, indexHeader), worksheet.Cell(3, indexHeader))
+                worksheet.Range(worksheet.Cell(rowHeader, columnHeader), worksheet.Cell(3, columnHeader))
                     .Merge()
                     .Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
 
-                worksheet.Cell(1, indexHeader).Style.Fill.SetBackgroundColor(XLColor.Yellow);
-                worksheet.Cell(1, indexHeader).Style.Font.SetBold();
-                worksheet.Cell(1, indexHeader).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-                worksheet.Cell(1, indexHeader).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                worksheet.Cell(rowHeader, columnHeader).Style.Fill.SetBackgroundColor(XLColor.Yellow);
+                worksheet.Cell(rowHeader, columnHeader).Style.Font.SetBold();
+                worksheet.Cell(rowHeader, columnHeader).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                worksheet.Cell(rowHeader, columnHeader).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
 
-                indexHeader++;
+                columnHeader++;
             }
             
-            int index = 4;
+            int rowBody = 4;
             foreach (var author in authors)
             {
-                worksheet.Cell(index, 1).Value = author.Id;
-                worksheet.Cell(index, 2).Value = author.FirstName;
-                worksheet.Cell(index, 3).Value = author.LastName;
+                worksheet.Cell(rowBody, 1).Value = author.Id;
+                worksheet.Cell(rowBody, 2).Value = author.FirstName;
+                worksheet.Cell(rowBody, 3).Value = author.LastName;
 
                 //styling
-                worksheet.Cell(index, 1).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-                worksheet.Cell(index, 2).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-                worksheet.Cell(index, 3).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                worksheet.Cell(rowBody, 1).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                worksheet.Cell(rowBody, 2).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                worksheet.Cell(rowBody, 3).Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
 
-                index++;
+                rowBody++;
             }
+            worksheet.Cell(rowBody, 1).FormulaA1 = "=SUM(A1:A" + (rowBody - 1) + ")"; //exp. =SUM(A1:A6)
+
 
             //styling
             worksheet.Columns(1, 3).AdjustToContents();
+            return workbook;
         }
     }
     public class Author  
